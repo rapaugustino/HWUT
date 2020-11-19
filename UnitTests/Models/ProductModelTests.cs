810c8c61ce758ab6daaf0db9fc4f23c333ce706e
Richard <rapaugustino@gmail.com>
2020-11-18 19:29:08 -0800

    Added Unit Tests

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -32,5 +32,102 @@ namespace UnitTests
             // Assert
             Assert.AreEqual(DateTime.UtcNow.ToShortDateString(), result.Date.ToShortDateString());
         }
+
+        [TestMethod]
+        public void AverageRating_Rating_isNull_shouldReturnZero()
+        {
+            // Arrange
+            var model = new ProductModel();
+            model.Ratings = null;
+            var expected = 0;
+
+
+            // Act
+            var actual = model.AverageRating();
+            
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AverageRating_countIsZero_ReturnZero()
+        {
+            // Arrange
+            var model = new ProductModel();
+            model.Ratings = new int[] { };
+            var expected = 0;
+
+
+            // Act
+            var actual = model.AverageRating();
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AverageRating_totalEqualsZeroReturnZero()
+        {
+            // Arrange
+            var model = new ProductModel();
+            model.Ratings = new int[] { -5, 5};
+            var expected = 0;
+
+
+            // Act
+            var actual = model.AverageRating();
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void AverageRating_happyPath()
+        {
+            // Arrange
+            var model = new ProductModel();
+            model.Ratings = new int[] {0, -3, 6};
+            var expected = 1;
+
+
+            // Act
+            var actual = model.AverageRating();
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void toString()
+        {
+            // Arrange
+            var model = new ProductModel();
+            model.Id = "4356";
+            model.Maker = "Rich";
+            model.Image = "flower";
+            model.Url = "rapaugustino.com";
+            model.Title = "Student";
+            model.Description = "CS";
+            model.Date = DateTime.Parse("11/18/2020");
+            model.Sequence = "Ten";
+            model.Email = "rap@seattleu.edu";
+            model.Logistics = "Get there by bus";
+            model.Ratings = new int[] { 2, 3, 4};
+            var expected = "{\"Id\":\"4356\",\"Maker\":\"Rich\",\"img\":\"flower\",\"Url\":\"rapaugustino.com\",\"Title\":\"Student\",\"Description\":\"CS\",\"Date\":\"2020-11-18T00:00:00\",\"Sequence\":\"Ten\",\"Email\":\"rap@seattleu.edu\",\"Logistics\":\"Get there by bus\",\"Ratings\":[2,3,4]}";
+
+
+
+            // Act
+            var actual = model.ToString();
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
